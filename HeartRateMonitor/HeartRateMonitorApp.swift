7869a664c4b47e493a146ac5b3d05245c515da49
Phoenix Perry <phoenix.perry@arts.ac.uk>
2025-03-05 15:20:08 +0000

    Initial Commit

---
+++ HeartRateMonitor/HeartRateMonitorApp.swift
@@ -0,0 +1,20 @@
+//
+//  HeartRateMonitorApp.swift
+//  HeartRateMonitor
+//
+//  Created by Phoenix Perry on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateMonitorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
